c7bf19832bf57d91cdef391b55e5063d9a2b27ad
Ariana <152944814+aam214@users.noreply.github.com>
2024-08-21 13:00:44 -0500

    Edited Bootstrap classes in h2. Edited body background-color. Edited margin-top to container. Removed button classes and only added one button class. Edited button transition and width. In JS dynamically added mp3 sounds classList and images. Click event listener added to buttons.

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const containerElement = document.querySelector(".container");
 
 drums.forEach(drum => {
 const instrumentButton = document.createElement("button");
-instrumentButton.classList.add("snare-button");
+instrumentButton.classList.add("music-button");
 instrumentButton.innerText = drum;
+instrumentButton.style.backgroundImage = "url(Images/" + drum + ".png)"
 containerElement.appendChild(instrumentButton);
+
+const soundElement = document.createElement("audio");
+soundElement.src = "sounds/" + drum + ".mp3"
+containerElement.appendChild(soundElement);
+
+instrumentButton.addEventListener("click", () => {
+soundElement.play();
+})
 });
